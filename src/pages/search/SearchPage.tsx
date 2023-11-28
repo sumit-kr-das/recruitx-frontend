49de0d9afb6a757b784fa46d34c61ae96151ef20
Shoyeb001 <shoyebjio3398@gmail.com>
2023-11-28 20:39:24 +0530

    search added with small change

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -6,60 +6,36 @@ import Job from "../../components/mnjuser/recomandedJobs/Job";
 import TopHeader from "../../components/navigation/TopHeader";
 import Footer from "../../components/footer/Footer";
 import { MdClose } from "react-icons/md";
+import MultiSelectInput from "../../components/form/multiSelectInput/MultiSelectInput";
+import SelectInput from "../../components/form/multiSelectInput/SelectInput";
+import { tagsData } from "../../constants/tagsData";
 const INITIAL_SEARCH_DATA = {
-  role:[""],
-  location:[""],
-  salary:0,
+  role: [""],
+  location: [""],
+  salary: 0,
 }
 
 const SearchPage = () => {
-  const [searchData, { isLoading }] = useSearchDataMutation();
   const [searchParams] = useSearchParams();
+  const title = searchParams.get("skills");
+  const exprience = Number(searchParams.get("exprience"));
+  const location = searchParams.get("location");
+  const [searchData, { isSuccess }] = useSearchDataMutation();
   const [jobs, setJobs] = useState([]);
   const [searchFilter, setSearchFilter] = useState();
 
-  const [role, setRole] = useState([]);
-  const [salary, setSalary] = useState();
-  const [locations, setLocation] = useState([]);
+  const [data, setData] = useState(INITIAL_SEARCH_DATA);
+  const [tags, setTags] = useState([title]);
 
 
 
   // const queryParams = new URLSearchParams(location.search);
   // const skill = queryParams.get("skill");
-  const title = searchParams.get("skills");
-  const exprience = Number(searchParams.get("exprience"));
-  const location = searchParams.get("location");
 
-  const handelRoles = (e) => {
-    if (e.key === "Enter") {
-      setRole([...role, e.target.value]);
-      console.log(role);
-    }
+  const handelFilterSubmit = (e) => {
+    e.preventDefault();
+    console.log(data, "searchFilter");
   };
-
-  const handelLocation = (e) => {
-    if (e.key === "Enter") {
-      setLocation([...locations, e.target.value]);
-      console.log(location);
-    }
-  };
-
-  const handelFilterSubmit = () => {
-    // const filters = {
-    //   role: role,
-    //   salary,
-    //   locations
-    // };
-    setSearchFilter({
-      role: role,
-      salary,
-      locations
-    });
-    console.log(searchFilter, "searchFilter");
-  };
-
-
-
 
   useEffect(() => {
     // const filters = JSON.parse(localStorage.getItem("filter") || 'null');
@@ -67,31 +43,27 @@ const SearchPage = () => {
     const fetchSearchData = async () => {
       try {
         const jobData = await searchData({ title, exprience, location }).unwrap();
-        const filteredJobs = jobData.filter(job => {
-          if (searchFilter) {
-            // Check if the job roles match the filter roles
-            const rolesMatch = searchFilter.role.every(role => job.info.skills.includes(role));
+        let filteredJobs;
+        if (isSuccess) {
+          filteredJobs = jobData.filter(job => {
+            if (data) {
+              const rolesMatch = data.role.every(role => job.info.skills.includes(role));
+              const salaryInRange = job.info.minSalary <= data.salary && job.info.maxSalary >= data.salary;
+              return rolesMatch || salaryInRange;
+            } else {
+              return jobData;
+            }
 
-            // Check if the job salary is within the filter salary range
-            const salaryInRange = job.info.minSalary <= searchFilter.salary && job.info.maxSalary >= searchFilter.salary;
+          });
+        }
 
-            // Check if the job location is in the filter locations
-            const locationMatch = searchFilter.locations.includes(job.info.location);
-
-            // Return true if all conditions are met, meaning the job passes the filter
-            return rolesMatch || salaryInRange || locationMatch;
-          } else {
-            return jobData;
-          }
-
-        });
         setJobs(filteredJobs);
       } catch (err) {
         console.log("Error on company login", err);
       }
     }
     fetchSearchData();
-  }, []);
+  }, [data]);
 
   return (
     <>
@@ -100,14 +72,14 @@ const SearchPage = () => {
         <section className="mt-32 w-full">
           <div className="flex items-center flex-col justify-center text-center">
             <h1 className="text-2xl font-bold">
-              {jobs.length} Jobs based on Your Desired Search
+              {jobs?.length} Jobs based on Your Desired Search
             </h1>
             <p className="mt-2">
               Exclusive opportunities based on what recruiters are searching for,
               <br /> even before they post a job on RecruitX
             </p>
           </div>
-          <div className="mt-10 flex justify-between">
+          <form className="mt-10 flex justify-between" onSubmit={handelFilterSubmit}>
             {/* left section */}
             <div className="w-7/12">
               <Job jobs={jobs} />
@@ -117,77 +89,49 @@ const SearchPage = () => {
               <div className="bg-white p-8 border rounded-lg">
                 <h1 className="text-xl font-bold">Manage your job preferences</h1>
                 <div className="mt-4">
-                  <p className="font-semibold">Preferred Job Role (Max 3)</p>
-                  <input
-                    className="border rounded-lg px-2 py-2 w-full my-2"
-                    type="text"
-                    placeholder="Enter your preferred job role"
-                    onKeyDown={handelRoles}
-
-                  />
-                  <div className="flex items-center gap-2 mt-1 flex-wrap">
-                    {role.map((item, index) => (
-                      <>
-                        <div className="flex items-center bg-slate-200 py-1 px-2 rounded-xl gap-1">
-                          <p className="text-sm font-semibold ">{item}</p>
-                          <MdClose
-                            className="text-xs cursor-pointer"
-                            onClick={() => setRole(role.filter((_, i) => i !== index))}
-                          />
-                        </div>
-                      </>
-                    ))}
-                    {/* <div className="flex items-center bg-slate-200 py-1 px-2 rounded-xl gap-1">
-                      <p className="text-sm font-semibold ">Software Programmer</p>
-                      <MdClose className="text-xs cursor-pointer" />
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="country"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Select Skills
+                    </label>
+                    <div className="mt-2 w-full">
+                      <SelectInput
+                        multiple
+                        options={tagsData}
+                        value={tags}
+                        onChange={(o) => {
+                          setTags(o);
+                          setData({ ...data, role: tags });
+                        }}
+                      />
                     </div>
-                    <div className="flex items-center bg-slate-200 py-1 px-2 rounded-xl gap-1">
-                      <p className="text-sm font-semibold ">Developer</p>
-                      <MdClose className="text-xs cursor-pointer" />
-                    </div> */}
                   </div>
+
                 </div>
                 <div className="mt-4">
-                  <p className="font-semibold">Preferred Salary</p>
-                  <input
-                    className="border rounded-lg px-2 py-2 w-full my-2"
-                    type="text"
-                    placeholder="Enter your preferred salary"
-                    onChange={(e) => setSalary(e.target.value)}
-                  />
-                  <div className="mt-4">
-                    <p className="font-semibold">Preferred Work Locations (Max 10)</p>
-                    <input
-                      className="border rounded-lg px-2 py-2 w-full my-2"
-                      type="text"
-                      placeholder="Enter your preferred location"
-                      onKeyDown={handelLocation}
-                    />
-                    <div className="flex items-center gap-2 mt-1 flex-wrap">
-                      {locations.map((item, index) => (
-                        <>
-                          <div className="flex items-center bg-slate-200 py-1 px-2 rounded-xl gap-1">
-                            <p className="text-sm font-semibold ">{item}</p>
-                            <MdClose
-                              className="text-xs cursor-pointer"
-                              onClick={() =>
-                                setLocation(locations.filter((_, i) => i !== index))
-                              }
-                            />
-                          </div>
-                        </>
-                      ))}
-                      <div className="flex items-center bg-slate-200 py-1 px-2 rounded-xl gap-1">
-                        <p className="text-sm font-semibold ">New Delhi</p>
-                        <MdClose className="text-xs cursor-pointer" />
-                      </div>
-                      <div className="flex items-center bg-slate-200 py-1 px-2 rounded-xl gap-1">
-                        <p className="text-sm font-semibold ">Kalkata</p>
-                        <MdClose className="text-xs cursor-pointer" />
-                      </div>
-                      <div className="flex items-center bg-slate-200 py-1 px-2 rounded-xl gap-1">
-                        <p className="text-sm font-semibold ">Guskara</p>
-                        <MdClose className="text-xs cursor-pointer" />
+                  <div className="sm:col-span-4">
+                    <label
+                      htmlFor="username"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Headline
+                    </label>
+                    <div className="mt-2">
+                      <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                        <input
+                          value={data.salary}
+                          onChange={(e) =>
+                            setData({ ...data, salary: e.target.value })
+                          }
+                          type="text"
+                          name="title"
+                          id="title"
+                          autoComplete="title"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="Enter a short headline"
+                        />
                       </div>
                     </div>
                   </div>
@@ -195,14 +139,14 @@ const SearchPage = () => {
                 <div className="flex items-center gap-4 mt-10">
                   <button
                     className="bg-cyan-500 text-white text-sm px-5 py-2 rounded-md hover:bg-cyan-600"
-                    onClick={handelFilterSubmit}
+                    type="submit"
                   >
                     Filter Search
                   </button>
                 </div>
               </div>
             </div>
-          </div>
+          </form>
         </section>
       </main >
       <Footer />
