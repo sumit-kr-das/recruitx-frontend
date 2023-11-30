f84bf6de9195548082517f3db730c3ddd0ea474e
sumit-kr-das <sumitkrdas777@gmail.com>
2023-11-30 22:58:25 +0530

    search on home page added

--- src/components/navigation/TopHeader.tsx
+++ src/components/navigation/TopHeader.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import MainLogo from "../../assets/logo.svg";
@@ -7,12 +8,15 @@ import {
 } from "../../features/auth/authSlice";
 import RadixMenu from "../../themes/RadixMenu";
 import { recruiterMenu, userMenu } from "../../constants/recruterMenu";
+import { Search, X } from "lucide-react";
+import SearchComponent from "../mnjuser/userHome/search/Search";
 
 const TopHeader = () => {
+  const [search, setSearch] = useState<boolean>(false);
   const user = useSelector(selectCurrentUser);
   const role = useSelector(selectCurrentRole);
   return (
-    <header className="bg-white shadow-sm fixed w-full z-20">
+    <header className=" bg-white shadow-sm fixed w-full z-20">
       <nav className="max-w-screen-xl mx-auto py-5 flex items-center justify-between">
         <div className="flex items-center gap-12">
           <Link to="/">
@@ -37,6 +41,36 @@ const TopHeader = () => {
             </li>
           </ul>
         </div>
+
+        {/* search */}
+        {role && role === "user" && (
+          <div className="relative mx-auto  cursor-pointer">
+            <div
+              onClick={() => setSearch((prev) => !prev)}
+              className="text-gray-400 flex items-center border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-full text-sm focus:outline-none"
+            >
+              Search
+            </div>
+            <button type="button" className="absolute right-0 top-0 mt-2 mr-4">
+              <Search className="w-[15px]" />
+            </button>
+          </div>
+        )}
+        {/* show/hide search */}
+        {search && (
+          <div className="absolute top-0 left-0 w-full">
+            <div className="bg-white py-5">
+              <SearchComponent />
+            </div>
+            <div className="bg-gradient-to-b from-white to-transparent h-[100px]"></div>
+            <div
+              onClick={() => setSearch((prev) => !prev)}
+              className="absolute top-[20%] right-[20%] cursor-pointer"
+            >
+              <X className="w-[40px]" />
+            </div>
+          </div>
+        )}
 
         <div className="flex items-center gap-4">
           {!user ? (
