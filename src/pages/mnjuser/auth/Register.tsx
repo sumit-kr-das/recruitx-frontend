12d9efd49c10117a313a6e6c7d88c3d05ac09ddd
sumit-kr-das <sumitkrdas777@gmail.com>
2024-01-24 22:32:55 +0530

    user-register init

--- src/pages/mnjuser/auth/Register.tsx
+++ src/pages/mnjuser/auth/Register.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import { AlertTriangle } from "lucide-react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { TApiError } from "../../../@types/TApiError";
 import { setCredentials } from "../../../features/auth/authSlice";
-import LoginBg from '../../../assets/bg.jpg'
 import { useUserRegisterMutation } from "../../../features/auth/user/userRegisterApiSlice";
+import { Button } from "../../../ui/button";
+import { Input } from "../../../ui/input";
 
-type TINITIAL_USER_STATE = {
+type FormValues = {
   name: string;
   email: string;
   phoneNo: string;
@@ -15,34 +17,22 @@ type TINITIAL_USER_STATE = {
   password: string;
 };
 
-const INITIAL_USER_STATE: TINITIAL_USER_STATE = {
-  name: "",
-  email: "",
-  phoneNo: "",
-  workStatus: "",
-  password: "",
-};
-
 const Register = () => {
-  const [user, setUser] = useState(INITIAL_USER_STATE);
   const [userRegister, { isLoading }] = useUserRegisterMutation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleUserValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormValues>({ mode: "all" });
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const submitForm: SubmitHandler<FormValues> = async (user) => {
     try {
       console.log(user);
       const userData = await userRegister(user).unwrap();
       dispatch(setCredentials(userData));
-      setUser(INITIAL_USER_STATE);
       toast.success("Register successfull");
       navigate("/mnjuser/home");
     } catch (err) {
@@ -51,206 +41,69 @@ const Register = () => {
     }
   };
   return (
-    <div className="bg-white">
-      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-          <img
-            alt="Night"
-            src={LoginBg}
-            className="absolute inset-0 h-full w-full object-cover opacity-80"
-          />
-
-          <div className="hidden lg:relative lg:block lg:p-12">
-            <Link className="block text-white" to="/">
-              <span className="sr-only">Home</span>
-              <img src="/vite.svg" alt="vite" />
-            </Link>
-
-            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-              Welcome to RecruitX
-            </h2>
-
-            <p className="mt-4 leading-relaxed text-white/90">
-              RecruitX help you get hired, faster: from preparing your CV,
-              getting recruiter attention, finding the right jobs, and more!
-            </p>
-          </div>
-        </section>
-
-        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-          <div className="max-w-xl lg:max-w-3xl">
-            <div className="relative -mt-16 block lg:hidden">
-              <Link
-                className="inline-flex h-16 w-16 items-center justify-center rounded-full bg-white text-blue-600 sm:h-20 sm:w-20"
-                to="/"
-              >
-                <span className="sr-only">Home</span>
-                <img src="/vite.svg" alt="vite" />
-              </Link>
-
-              <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                Welcome to RecruitX
-              </h1>
-
-              <p className="text-sm mt-4 leading-relaxed text-gray-500">
-                RecruitX help you get hired, faster: from preparing your CV,
-                getting recruiter attention, finding the right jobs, and more!
-              </p>
-            </div>
-
-            <form
-              onSubmit={handleSubmit}
-              className="mt-8 grid grid-cols-6 gap-6"
+    <>
+      <div className="pt-40 w-full h-screen flex justify-center">
+        <div className="w-96">
+          <h1 className="text-4xl font-extrabold text-center">
+            Login Your <br /> RecruitX Account
+          </h1>
+          <form onSubmit={handleSubmit(submitForm)}>
+            <Input
+              type="email"
+              placeholder="Email"
+              className="mt-6"
+              {...register("email", {
+                required: "Email address is required",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Entered value does not match email format",
+                },
+              })}
+            />
+            {errors.email && (
+              <div className="mt-2 flex items-center gap-2 text-red-600">
+                <AlertTriangle className="h-4 w-4" />
+                {typeof errors.email.message === "string" && (
+                  <p>{errors.email.message}</p>
+                )}
+              </div>
+            )}
+            <Input
+              type="password"
+              placeholder="Password"
+              className="mt-4"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 5,
+                  message: "Password should be min 5 character log",
+                },
+              })}
+            />
+            {errors.password && (
+              <div className="mt-2 flex items-center gap-2 text-red-600">
+                <AlertTriangle className="h-4 w-4" />
+                {typeof errors.password.message === "string" && (
+                  <p>{errors.password.message}</p>
+                )}
+              </div>
+            )}
+            <Button
+              type="submit"
+              disabled={!isValid || isLoading}
+              className="w-full mt-4"
             >
-              <div className="col-span-6 sm:col-span-3">
-                <label
-                  htmlFor="FirstName"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Full name
-                </label>
-
-                <input
-                  required
-                  value={user.name}
-                  onChange={handleUserValue}
-                  type="text"
-                  id="FirstName"
-                  name="name"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label
-                  htmlFor="LastName"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Phone no
-                </label>
-
-                <input
-                  required
-                  value={user.phoneNo}
-                  onChange={handleUserValue}
-                  type="text"
-                  id="phoneNo"
-                  name="phoneNo"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6">
-                <label
-                  htmlFor="Email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Work status
-                </label>
-
-                <select
-                  required
-                  value={user.workStatus}
-                  onChange={handleUserValue}
-                  name="workStatus"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                >
-                  <option value="">Select status</option>
-                  <option value="experienced">I'm experienced</option>
-                  <option value="fresher">I'm a fresher</option>
-                </select>
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label
-                  htmlFor="Email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email
-                </label>
-
-                <input
-                  required
-                  value={user.email}
-                  onChange={handleUserValue}
-                  type="email"
-                  id="Email"
-                  name="email"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label
-                  htmlFor="Password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-
-                <input
-                  required
-                  value={user.password}
-                  onChange={handleUserValue}
-                  type="password"
-                  id="Password"
-                  name="password"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6">
-                <label htmlFor="MarketingAccept" className="flex gap-4">
-                  <input
-                    required
-                    type="checkbox"
-                    id="MarketingAccept"
-                    name="marketing_accept"
-                    className="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
-                  />
-
-                  <span className="text-sm text-gray-700">
-                    I want to receive emails about events, product updates and
-                    company announcements.
-                  </span>
-                </label>
-              </div>
-
-              <div className="col-span-6">
-                <p className="text-sm text-gray-500">
-                  By creating an account, you agree to our
-                  <a href="#" className="text-gray-700 underline">
-                    terms and conditions
-                  </a>
-                  and
-                  <a href="#" className="text-gray-700 underline">
-                    privacy policy
-                  </a>
-                  .
-                </p>
-              </div>
-
-              <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
-                <button
-                  type="submit"
-                  className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
-                >
-                  {isLoading ? "Loading..." : "Register"}
-                </button>
-
-                <p className="mt-4 text-sm text-gray-500 sm:mt-0">
-                  Don't have an account?{" "}
-                  <Link to="/mnjuser/login" className="text-gray-700 underline">
-                    Log in
-                  </Link>
-                  .
-                </p>
-              </div>
-            </form>
+              Log In
+            </Button>
+          </form>
+          <div className="mt-2 float-right font-medium text-blue-600">
+            <Link to={`/reset-password`}>
+              <p>Forgot password? Click here</p>
+            </Link>
           </div>
-        </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
