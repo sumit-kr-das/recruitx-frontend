d147fc5bb1f2b1671a093c885811e5881826ed5b
sumit-kr-das <sumitkrdas777@gmail.com>
2024-01-25 12:24:30 +0530

    user-register api add

--- src/pages/mnjuser/auth/Register.tsx
+++ src/pages/mnjuser/auth/Register.tsx
@@ -1,6 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import * as z from "zod";
@@ -27,6 +26,15 @@ import { Button } from "../../../ui/button";
 import { Input } from "../../../ui/input";
 import TopHeader from "../../../components/navigation/TopHeader";
 import UserRegisterSchema from "../../../@types/zod/UserRegisterSchema";
+import { toast } from "../../../ui/use-toast";
+
+type FormValues = {
+  name: string;
+  email: string;
+  phoneNo: string;
+  password: string;
+  confirmPassword: string;
+};
 
 const Register = () => {
   const [userRegister, { isLoading }] = useUserRegisterMutation();
@@ -40,122 +48,151 @@ const Register = () => {
       email: "",
       phoneNo: "",
       password: "",
+      confirmPassword: ""
     },
   });
 
-  // const submitForm: SubmitHandler<FormValues> = async (value) => {
-  //   try {
-  //     console.log(value);
-  //     const userData = await userRegister(value).unwrap();
-  //     dispatch(setCredentials(userData));
-  //     toast.success("Register successfull");
-  //     navigate("/mnjuser/home");
-  //   } catch (err) {
-  //     const apiError = err as TApiError;
-  //     toast.error(apiError?.data.message);
-  //   }
-  // };
+  const submitForm: SubmitHandler<FormValues> = async (value) => {
+    try {
+      console.log(value);
+      const userData = await userRegister(value).unwrap();
+      dispatch(setCredentials(userData));
+      toast({
+        description: "User registered successfully",
+      });
+      navigate("/mnjuser/home");
+    } catch (err) {
+      const apiError = err as TApiError;
+      toast({
+        variant: "destructive",
+        description: apiError?.data.message,
+      });
+    }
+  };
 
-  function onSubmit() {}
   return (
-    <>
+    <div className="bg-cyan-50">
       <TopHeader />
       <div className="pt-40 w-full h-screen flex justify-center">
-        <div className="w-[800px]">
-          <h1 className="text-4xl mb-10 font-extrabold text-center">
-            Login Your <br /> RecruitX Account
+        <div className="w-[800px] h-fit rounded-xl bg-white p-10 shadow">
+          <h1 className="text-2xl mb-10 font-extrabold text-center">
+            Register Your RecruitX Account
           </h1>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Full name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Your full name" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="phoneNo"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Phone no</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Your phone no" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Your email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Your email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="workStatus"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Select your status</FormLabel>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
+            <form
+              onSubmit={form.handleSubmit(submitForm)}
+              className="space-y-8"
+            >
+              <div className="flex gap-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Full name</FormLabel>
                       <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Select ststus" />
-                        </SelectTrigger>
+                        <Input placeholder="Your full name" {...field} />
                       </FormControl>
-                      <SelectContent>
-                        <SelectItem value="student">I am a student</SelectItem>
-                        <SelectItem value="employee">
-                          I am an employee
-                        </SelectItem>
-                        <SelectItem value="other">Other</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Your password"
-                        type="password"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <Button className="w-full" type="submit">
-                Login Now
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="phoneNo"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Phone no</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Your phone no" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex gap-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Your email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Your email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="workStatus"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Select your status</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select status" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="student">
+                            I am a student
+                          </SelectItem>
+                          <SelectItem value="employee">
+                            I am an employee
+                          </SelectItem>
+                          <SelectItem value="other">Other</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex gap-4">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Your password"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Confirm password</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Confirm password"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button disabled={isLoading} className="w-full" type="submit">
+                Register Now
               </Button>
 
               <FormDescription>
@@ -165,7 +202,7 @@ const Register = () => {
           </Form>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
