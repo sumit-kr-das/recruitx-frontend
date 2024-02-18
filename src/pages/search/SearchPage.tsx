7854a051cb3587f3879186a8f5665f178514acb5
sumit-kr-das <sumitkrdas777@gmail.com>
2024-02-18 11:23:24 +0530

    search page

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,30 +1,27 @@
-import React from "react";
+import Footer from "../../components/footer/Footer";
 import TopHeader from "../../components/navigation/TopHeader";
-import Search from "../../components/mnjuser/userHome/search/Search";
 import Container from "../../layout/Container";
-import ApplyFilter from "./_components/ApplyFilter";
+import Filter from "./_components/Filter";
 import FilteredJobs from "./_components/FilteredJobs";
-import FilterJobDetails from "./_components/FilterJobDetails";
 
 const SearchPage = () => {
   return (
-    <div className="bg-[#FAFAFA] h-screen">
+    <>
       <TopHeader />
-      <Container className="pt-24">
-        <Search />
-        <div className="relative mt-8 flex justify-between h-fit">
-          <div className="w-[20%] h-screen absolute top-0 left-0 bg-white p-4 rounded-lg border shadow ">
-            <ApplyFilter />
-          </div>
-          <div className="border border-green-200 bg-yellow-100 w-[40%] h-fit">
-            <FilteredJobs />
-          </div>
-          <div className="border border-blue-200 bg-orange-100 w-[40%] h-fit">
-            <FilterJobDetails />
+      <Container className="pt-20 ">
+        <div className="max-h-screen flex flex-col">
+          <div className="grid grid-cols-[auto,1fr] flex-grow-1  sticky top-0 overflow-y-auto scrollbar-hidden">
+            <Filter />
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))] scrollbar-hidden sticky top-0 overflow-y-auto scrollbar-hidden">
+              {[...Array(50)].map((_, index) => (
+                <FilteredJobs key={index} />
+              ))}
+            </div>
           </div>
         </div>
       </Container>
-    </div>
+      <Footer />
+    </>
   );
 };
 
