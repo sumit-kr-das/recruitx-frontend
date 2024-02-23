f0efe9f92294067ea011c86cd39e698e97590cd2
sumit-kr-das <sumitkrdas777@gmail.com>
2024-02-23 20:18:52 +0530

    bg gradient added

--- src/components/visitors/Visitors.tsx
+++ src/components/visitors/Visitors.tsx
@@ -10,15 +10,21 @@ const Visitors = ({ data }: TStatProp) => {
       <div className="flex items-center justify-center gap-8">
         <div className="flex items-center flex-col">
           <h2 className="font-semibold">Total Users</h2>
-          <h1 className="text-9xl font-bold">{data?.users}</h1>
+          <h1 className="text-9xl font-bold bg-gradient-to-r from-teal-200 to-teal-500 bg-clip-text text-transparent">
+            {data?.users}
+          </h1>
         </div>
         <div className="flex items-center flex-col">
           <h2 className="font-semibold">Total Companies</h2>
-          <h1 className="text-9xl font-bold">{data?.companies}</h1>
+          <h1 className="text-9xl font-bold bg-gradient-to-r from-teal-200 to-teal-500 bg-clip-text text-transparent">
+            {data?.companies}
+          </h1>
         </div>
         <div className="flex items-center flex-col">
           <h2 className="font-semibold">Total Jobs</h2>
-          <h1 className="text-9xl font-bold">{data?.jobs}</h1>
+          <h1 className="text-9xl font-bold bg-gradient-to-r from-teal-200 to-teal-500 bg-clip-text text-transparent">
+            {data?.jobs}
+          </h1>
         </div>
       </div>
     </div>
