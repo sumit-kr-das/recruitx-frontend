9d5335e910596b8ec298edb831ec4fd40b859c2e
sumit-kr-das <sumitkrdas777@gmail.com>
2024-02-28 21:47:44 +0530

    applied job page updated

--- src/pages/mnjuser/ViewAppliedPage.tsx
+++ src/pages/mnjuser/ViewAppliedPage.tsx
@@ -5,43 +5,34 @@ import TopHeader from "../../components/navigation/TopHeader";
 import Container from "../../layout/Container";
 import AppliedJob from "../../components/mnjuser/AppliedJob";
 import { useViewAppliedJobsQuery } from "../../features/user/get/viewAppliedJobs";
+import Loader from "../../components/loader/Loader";
 
 const ViewAppliedPage = () => {
-    const { data, isLoading } = useViewAppliedJobsQuery();
-    return (
-        <>
-            <div className="bg-green-50">
-                <TopHeader />
-                <Container className="flex justify-between">
-                    <section className="bg-white border rounded-md w-[240px] h-fit mt-24">
-                        <UserProfile />
-                    </section>
-                    {
-                        isLoading ? (<>
-                            <section className="flex-1 h-fit mt-24 px-4">
-                                Loading...
-                            </section>
-                        </>) : (<>
-                            <section className="flex-1 h-fit mt-24 px-4 w-7/12">
-                                {
-                                    data?.map((item, index) => (
-                                        <>
-                                            <AppliedJob job={item} key={index} />
-                                        </>
-                                    ))
-                                }
+  const { data, isLoading } = useViewAppliedJobsQuery();
 
-                            </section>
-                        </>)
-                    }
-                    <section className="w-[240px] mt-24">
-                        <UserNotices />
-                    </section>
-                </Container>
-                <Footer />
-            </div>
-        </>
-    )
-}
+  return (
+    <div className="bg-[#FAFAFA]">
+      <TopHeader />
+      <Container className="flex justify-between">
+        <section className="bg-white border rounded-md w-[240px] h-fit mt-24">
+          <UserProfile />
+        </section>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <section className="flex-1 h-fit mt-24 px-4 w-7/12">
+            {data?.map((item, index) => (
+              <AppliedJob job={item} key={index} />
+            ))}
+          </section>
+        )}
+        <section className="w-[240px] mt-24">
+          <UserNotices />
+        </section>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
 
-export default ViewAppliedPage
+export default ViewAppliedPage;
