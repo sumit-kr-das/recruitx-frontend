e5b0c42d921a69fa5ec488e1f69d834cffab79e3
sumit-kr-das <sumitkrdas777@gmail.com>
2024-03-12 21:41:43 +0530

    user home search page debouncing implement

--- src/components/mnjuser/userHome/search/Search.tsx
+++ src/components/mnjuser/userHome/search/Search.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "../../../../ui/button";
 import {useLazySearchJobsTitleQuery} from "../../../../features/user/get/searchJobsTitleApiSlice.ts";
 import {useDebounce} from "../../../../customHooks/useDebounce.ts";
+import {citiesArray} from "../../../../constants/citiesArray.ts";
 
 const Search = () => {
   const navigate = useNavigate();
@@ -11,6 +12,7 @@ const Search = () => {
     skill: "",
     location: "",
   });
+  const [showCitySuggestions, setShowCitySuggestions] = useState(true);
   const [titleData, setTitleData] = useState([]);
   const [trigger, { data: searchData }] = useLazySearchJobsTitleQuery();
   const debounceSearch = useDebounce(data.skill, 1000);
@@ -29,17 +31,26 @@ const Search = () => {
       setTitleData(searchData);
   }, [debounceSearch, searchData]);
 
+  function setSkills (skillData: string) {
+    setData({...data, skill: skillData})
+    setTitleData([]);
+  }
+
+  function setCities (locationData: string) {
+    setData({...data, location: locationData})
+    setShowCitySuggestions(false);
+  }
 
   return (
     <div className="w-full flex items-center justify-center">
       <div className="relative bg-white md:w-8/12 flex items-center justify-center gap-4 border-2 rounded-lg shadow px-3 py-2">
-        <div className=" flex items-center justify-center gap-1 flex-1">
+        <div className="flex items-center justify-center gap-1 flex-1">
           <SearchIcn className="w-4 h-4 text-gray-400"/>
           <input
               type="text"
               placeholder="Business Development Manager"
               className="block w-full rounded-md border-0 py-1.5 focus:outline-none focus:ring-transparent"
-              name="skill"
+              value={data.skill}
               onChange={(e) => setData({...data, skill: e.target.value})}
           />
         </div>
@@ -47,11 +58,13 @@ const Search = () => {
             titleData?.length > 0 &&
             <div className="w-full mb-2 absolute z-50 top-16 left-0 bg-white rounded-lg border">
               {
-                titleData?.map((item, index) => (
+                titleData?.map((item: {title: string}, index) => (
                     <div key={index}
-                         className="text-gray-500 flex items-center gap-2 px-3 py-2 hover:bg-gray-200 cursor-pointer">
+                         onClick={() =>setSkills(item?.title)}
+                         className="text-gray-500 flex items-center gap-2 px-3 py-2 hover:bg-gray-200 cursor-pointer"
+                    >
                       <SearchIcn className="w-5 h-5 text-gray-400"/>
-                      <p className="text-lg ">{item?.title}</p>
+                      <p className="md:text-lg">{item?.title}</p>
                     </div>
                 ))
               }
@@ -63,11 +76,29 @@ const Search = () => {
           <input
               type="text"
               placeholder="London, United Kingdom"
-              className="block w-full rounded-md border-0 py-1.5 focus:outline-none focus:ring-transparent"
-              name="location"
+              className="block w-1/2 rounded-md border-0 py-1.5 focus:outline-none focus:ring-transparent"
+              value={data.location}
               onChange={(e) => setData({...data, location: e.target.value})}
           />
         </div>
+        {showCitySuggestions && data.location && (
+            <div className="w-full mb-2 absolute z-50 top-16 left-0 bg-white rounded-lg border">
+              {citiesArray
+                  .filter((city) =>
+                      city.label.toLowerCase().includes(data.location.toLowerCase())
+                  )
+                  .map((item, index) => (
+                      <div
+                          key={index}
+                          className="text-gray-500 flex items-center gap-2 px-3 py-2 hover:bg-gray-200 cursor-pointer"
+                          onClick={() => setCities(item.label)}
+                      >
+                        <MapPin className="w-5 h-5 text-gray-400" />
+                        <p className="md:text-lg">{item.label}</p>
+                      </div>
+                  ))}
+            </div>
+        )}
         <Button className="bg-cyan-500 hover:bg-cyan-600" onClick={search}>
           Search
         </Button>
