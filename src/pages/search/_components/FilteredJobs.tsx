4eff4a5a802cef618b95193d3aad9cb82c516292
shoyeb001 <shoyebjio3398@gmail.com>
2024-03-12 19:02:39 +0530

    job page error solved

--- src/pages/search/_components/FilteredJobs.tsx
+++ src/pages/search/_components/FilteredJobs.tsx
@@ -73,30 +73,30 @@ const FilteredJobs = ({ job }: JobProps) => {
       <CardHeader className="flex items-center flex-row gap-4">
         <img
           className="w-16 h-16 rounded-full  object-cover border"
-          src={job.companyId.companyProfileId.logo || DefaultCompany}
+          src={job?.companyId?.companyProfileId?.logo || DefaultCompany}
           alt="company logo"
         />
         <div>
-          <h1 className="text-xl font-semibold line-clamp-1">{job.title}</h1>
-          <p>{job.companyId.companyName}</p>
+          <h1 className="text-xl font-semibold line-clamp-1">{job?.title}</h1>
+          <p>{job?.companyId?.companyName}</p>
         </div>
       </CardHeader>
       <CardContent className="h-full flex flex-col justify-between">
         <div className="flex items-center flex-wrap gap-2 my-2">
           <p className="bg-blue-50 text-blue-500 text-sm font-semibold px-4 py-2 rounded-lg">
-            {job.info.jobType}
+            {job?.info?.jobType}
           </p>
           <p className="bg-red-50 text-red-500 text-sm font-semibold px-4 py-2 rounded-lg">
-            {job.info.minExprience}-{job.info.maxExprience} Year
+            {job?.info?.minExprience}-{job?.info?.maxExprience} Year
           </p>
           <p className="bg-green-50 text-green-500 text-sm font-semibold px-4 py-2 rounded-lg">
-            {job.info.location}
+            {job?.info?.location}
           </p>
           <p className="bg-cyan-50 text-cyan-500 text-sm font-semibold px-4 py-2 rounded-xl">
-            {job.info.workplaceType}
+            {job?.info?.workplaceType}
           </p>
         </div>
-        <p className="line-clamp-2 mt-2">{job.description}</p>
+        <p className="line-clamp-2 mt-2">{job?.description}</p>
       </CardContent>
       <CardFooter className="w-full flex items-center flex-col justify-between">
         <Separator className="my-4" />
@@ -104,10 +104,15 @@ const FilteredJobs = ({ job }: JobProps) => {
           <div className="flex items-center gap-1">
             <IndianRupee className="w-4 h-4" />
             <p>
-              <span className="font-semibold">{job.info.minSalary} </span>LPA
+              <span className="font-semibold">{job?.info?.minSalary} </span>LPA
             </p>
           </div>
-          <Button className="bg-cyan-500 hover:bg-cyan-600" onClick={() => applyForJob(job?._id)}>Apply Now</Button>
+          <Button
+            className="bg-cyan-500 hover:bg-cyan-600"
+            onClick={() => applyForJob(job?._id)}
+          >
+            Apply Now
+          </Button>
         </div>
       </CardFooter>
     </Card>
