a75f484549a589a37c11b17530005c45dbd462e2
sumit-kr-das <sumitkrdas777@gmail.com>
2024-03-14 14:45:00 +0530

    home stat skeliton updated

--- src/pages/user/_components/PublicStats.tsx
+++ src/pages/user/_components/PublicStats.tsx
@@ -6,23 +6,27 @@ const PublicStats = () => {
   const { data, isLoading } = useGetWebStatsQuery();
 
   return (
-      <div>
-        <div className="mx-auto max-w-xl text-center mt-20 mb-10">
-          <h1 className="text-2xl md:text-4xl font-extrabold">
-            Trusted by <span className="text-cyan-500">Jobseekers & Companies</span>
-          </h1>
-          <p className="mt-4 sm:text-xl/relaxed">Recruitx has a greate achivement of stats and proudly show it how we are chaning the hiring industry.</p>
-        </div>
-        {data || !isLoading ? (
-            <>{data && <Visitors data={data}/>}</>
-        ) : (
-            <>
-              {[...Array(3)].map((_, index) => (
-                  <PublicStatsSkeletion key={index}/>
-              ))}
-            </>
-        )}
+    <div>
+      <div className="mx-auto max-w-xl text-center mt-20 mb-10">
+        <h1 className="text-2xl md:text-4xl font-extrabold">
+          Trusted by{" "}
+          <span className="text-cyan-500">Jobseekers & Companies</span>
+        </h1>
+        <p className="mt-4 sm:text-xl/relaxed">
+          Recruitx has a greate achivement of stats and proudly show it how we
+          are chaning the hiring industry.
+        </p>
       </div>
+      {data || !isLoading ? (
+        <>{data && <Visitors data={data} />}</>
+      ) : (
+        <div className="flex items-center justify-center  gap-8">
+          {[...Array(3)].map((_, index) => (
+            <PublicStatsSkeletion key={index} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
